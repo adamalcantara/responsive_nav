d4ca8ad431ae721f055c31bfdcfacd39888a9c40
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-07 18:55:01 -0400

    Styling and animation applied

--- app.js
+++ app.js
@@ -6,16 +6,23 @@ const navSlide = () => {
     //Toggle the navigation
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        //Animate the links
+        navLinks.forEach((link, index) => {
+            //Console.log index, should get 4
+            console.log(index)
+            //Add animation, index divided by 7 so that they animate individually
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                console.log(index / 7);
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
     });
 
-    //Animate the links
-    navLinks.forEach((link, index)=>{
-        //Console.log index, should get 4
-        console.log(index)
-        //Add animation, index divided by 7 so that they animate individually
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
-        console.log(index/7);
-    })
+
 }
 
 const app = () => {
